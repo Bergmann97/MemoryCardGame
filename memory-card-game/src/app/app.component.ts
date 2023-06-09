8896c23ab06a4c0ada07f7133df262569caf879c
Bergmann97 <max.bergmann@b-tu.de>
2023-06-09 17:00:06 +0200

    added full app into template and gave style

--- memory-card-game/src/app/app.component.ts
+++ memory-card-game/src/app/app.component.ts
@@ -1,15 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     CommonModule,
+    HomeComponent,
   ],
   template: `
   <main>
     <section class="content">
+      <app-home></app-home>
     </section>
   </main>
   `,
